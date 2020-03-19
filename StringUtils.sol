18170fe4353aca36e9822c43657590bdc382b772
Ihab E. McShea <ihabshea@gmail.com>
2020-03-19 17:41:11 +0200

    String Utils for Solidity>=0.5

---
+++ StringUtils.sol
@@ -0,0 +1,45 @@
+library StringUtils {
+    function compare(string memory _a, string memory _b) returns (int256) {
+        bytes memory a = bytes(_a);
+        bytes memory b = bytes(_b);
+        uint256 minLength = a.length;
+        if (b.length < minLength) minLength = b.length;
+        for (uint256 i = 0; i < minLength; i++)
+            if (a[i] < b[i]) return -1;
+            else if (a[i] > b[i]) return 1;
+        if (a.length < b.length) return -1;
+        else if (a.length > b.length) return 1;
+        else return 0;
+    }
+    function equal(string memory _a, string memory _b) public returns (bool) {
+        return compare(_a, _b) == 0;
+    }
+    function indexOf(string memory _haystack, string memory _needle)
+        public
+        returns (int256)
+    {
+        bytes memory h = bytes(_haystack);
+        bytes memory n = bytes(_needle);
+        if (h.length < 1 || n.length < 1 || (n.length > h.length)) return -1;
+        else if (h.length > (2**128 - 1))
+            return -1;
+        else {
+            uint256 subindex = 0;
+            for (uint256 i = 0; i < h.length; i++) {
+                if (h[i] == n[0]) // found the first char of b
+                {
+                    subindex = 1;
+                    while (
+                        subindex < n.length &&
+                        (i + subindex) < h.length &&
+                        h[i + subindex] == n[subindex] 
+                    ) {
+                        subindex++;
+                    }
+                    if (subindex == n.length) return int256(i);
+                }
+            }
+            return -1;
+        }
+    }
+}
